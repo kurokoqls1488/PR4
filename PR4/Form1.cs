98e1317026057ecdcca10cb7197c4e553362d723
Шиняева Екатерина <katya.shinyaeva0@gmail.com>
2024-12-26 08:37:44 +0500

    f

--- PR4/Form1.cs
+++ PR4/Form1.cs
@@ -6,5 +6,10 @@ namespace PR4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ffgfg
+        }
     }
 }
